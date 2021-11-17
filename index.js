660a05ed64445cca64b453d8ac2a0a5143f85316
rejaulrasel <rejaulhasanrasel28@gmail.com>
2021-11-18 01:08:58 +0600

    added setup

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')//cors for own server connected with own
 const { MongoClient } = require('mongodb');
 require("dotenv").config();//dotenv config
 const app = express()
-const port = 8000
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 8000
+
+//Middleware
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f2uob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -14,7 +20,24 @@ console.log(uri);
 async function run(){
     try{
         await client.connect();
-        console.log('yes database connecteds')
+        console.log('yes database connected')
+
+        const database = client.db("Car-Mania");
+        const usersCollection = database.collection("user");
+        const servicesCollection = database.collection("services");
+        const eventsCollection = database.collection("events");
+        const reviewsCollection = database.collection("reviews");
+
+
+        //getServices
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      // console.log(services);
+      res.send(services);
+    })
+
+
     }
     finally{
         // await client.close();
